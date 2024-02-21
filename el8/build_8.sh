764141be392d9ecd1ab03c07514d9a5d2496d0f4
zuki <zuki@work>
2024-02-21 11:46:06 +0630

    changed build_8.sh

--- el8/build_8.sh
+++ el8/build_8.sh
@@ -35,5 +35,5 @@ cp -f $target_folder ./src/$src_folder_name
 cd ./src/
 tar -cvzf ../rpmbuild/SOURCES/$src_folder_name.tar.gz $src_folder_name/
 
-# docker exec -it --user root rpm_env chown -R moon.moon /home/moon/rpmbuild/
-# docker exec -it --user moon rpm_env rpmbuild -ba /home/moon/rpmbuild/SPECS/jobarranger8.spec
+docker exec -it --user root rpm_env chown -R moon.moon /home/moon/rpmbuild/
+docker exec -it --user moon rpm_env rpmbuild -ba /home/moon/rpmbuild/SPECS/jobarranger8.spec
