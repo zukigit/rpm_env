deb236c4905e931e7247e9cf28f74f059f01700b
zuki <zuki@work>
2024-02-21 13:04:15 +0630

    added srcs

--- compose-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker compose -f ./el6/docker-compose.yml up -d
-docker compose -f ./el7/docker-compose.yml up -d
-docker compose -f ./el8/docker-compose.yml up -d
-docker compose -f ./el9/docker-compose.yml up -d
